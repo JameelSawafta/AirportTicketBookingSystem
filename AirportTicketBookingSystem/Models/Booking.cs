61638806627b924147af340b5017ac263b0d62b5
Jameel Sawafta <jameel.wlar999@gmail.com>
2024-09-04 01:34:56 +0300

    Update Booking Model

--- AirportTicketBookingSystem/Models/Booking.cs
+++ AirportTicketBookingSystem/Models/Booking.cs
@@ -8,13 +8,23 @@ public class Booking
     public Guid BookingId;
     
     [Required]
-    public Guid FlightId;
+    public Guid FlightId { get; set; }
     
     [Required]
-    public Guid PassengerId;
+    public Guid PassengerId { get; set; }
+
+    [Required]
+    public FlightClass FlightClass { get; set; }
     
-    
-    
-    
-    
+    [Required]
+    public DateTime BookingDate { get; set; }
+
+    public Booking(Guid flightId, Guid passengerId, FlightClass flightClass, DateTime bookingDate)
+    {
+        BookingId = new Guid();
+        FlightId = flightId;
+        PassengerId = passengerId;
+        FlightClass = flightClass;
+        BookingDate = bookingDate;
+    }
 }
