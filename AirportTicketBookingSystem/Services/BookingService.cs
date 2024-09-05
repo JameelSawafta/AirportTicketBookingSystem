d252f5449417d63f54a3971f3b06045604f0384e
Jameel Sawafta <jameel.wlar999@gmail.com>
2024-09-05 23:22:53 +0300

    Add BookingService

---
+++ AirportTicketBookingSystem/Services/BookingService.cs
@@ -0,0 +1,45 @@
+using AirportTicketBookingSystem.Interfaces;
+using AirportTicketBookingSystem.Models;
+
+namespace AirportTicketBookingSystem.Services;
+
+public class BookingService : IBookingService
+{
+    private readonly List<Booking> _bookings;
+
+    public BookingService(List<Booking> bookings)
+    {
+        _bookings = bookings;
+    }
+
+    
+    public void CreateBooking(Booking booking)
+    {
+        _bookings.Add(booking);
+    }
+
+    public void DeleteBooking(Guid BookingId)
+    {
+        var _booking = _bookings.FirstOrDefault(b => b.BookingId == BookingId);
+        if (_booking != null)
+        {
+            _bookings.Remove(_booking);
+        }
+    }
+
+    public void UpdateBooking(Booking booking)
+    {
+        var _booking = _bookings.FirstOrDefault(b => b.BookingId == booking.BookingId);
+        if (_booking != null)
+        {
+            _booking.FlightId = booking.FlightId;
+            _booking.FlightClass = booking.FlightClass;
+            _booking.BookingDate = booking.BookingDate;
+        }
+    }
+
+    public IEnumerable<Booking> GetAllBookings()
+    {
+        return _bookings;
+    }
+}
