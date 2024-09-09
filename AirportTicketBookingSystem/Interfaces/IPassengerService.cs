68f9e009f75f4590554800cabbb714569f6bd7a0
Jameel Sawafta <jameel.wlar999@gmail.com>
2024-09-09 10:39:28 +0300

    Add IPassengerService and PassengerService

---
+++ AirportTicketBookingSystem/Interfaces/IPassengerService.cs
@@ -0,0 +1,9 @@
+using AirportTicketBookingSystem.Models;
+
+namespace AirportTicketBookingSystem.Interfaces;
+
+public interface IPassengerService
+{
+    public Passenger PassengerSignIn(string email, string password);
+    public Passenger CreatePassenger(Passenger passenger);
+}
---
+++ AirportTicketBookingSystem/Services/PassengerService.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using AirportTicketBookingSystem.Interfaces;
+using AirportTicketBookingSystem.Models;
+
+namespace AirportTicketBookingSystem.Services;
+
+public class PassengerService : IPassengerService
+{
+    private readonly List<Passenger> _passengers;
+
+    public PassengerService(List<Passenger> passengers)
+    {
+        _passengers = passengers;
+    }
+
+
+    public Passenger PassengerSignIn(string email, string password)
+    {
+        var passenger = _passengers.FirstOrDefault(p => p.Email == email && p.Password == password);
+        
+        if (passenger == null)
+        {
+            Console.WriteLine("Invalid email or password."); 
+        }
+        
+        return passenger;
+    }
+
+    public Passenger CreatePassenger(Passenger passenger)
+    {
+        ValidationService.Validation(passenger,x => _passengers.Add((Passenger)x));
+        return passenger;
+    }
+}
